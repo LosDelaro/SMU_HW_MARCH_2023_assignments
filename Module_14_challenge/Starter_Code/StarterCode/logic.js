c41c58ffe7f04c7c9cf1249e43ad04920077de04
LosDelaro <cdelarosa25@yahoo.com>
2023-07-19 21:46:15 -0500

    Module 14 Challenge

--- Module_14_challenge/Starter_Code/StarterCode/logic.js
+++
@@ -1,8 +0,0 @@
-// setup the URL to read in the JSON data 
-let baseURL =  'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
-
-// read in the sampls.json file 
-d3.json(baseURL).then(function(data) {
-    console.log(data);
-  });
-  
