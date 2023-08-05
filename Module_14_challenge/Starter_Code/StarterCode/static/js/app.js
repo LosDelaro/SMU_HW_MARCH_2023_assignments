ad25348599869fd30c2f65e141e2d264cde03705
LosDelaro <cdelarosa25@yahoo.com>
2023-08-04 23:00:55 -0500

    Completed HW 14

--- Module_14_challenge/Starter_Code/StarterCode/static/js/app.js
+++ Module_14_challenge/Starter_Code/StarterCode/static/js/app.js
@@ -1,12 +1,14 @@
 // setup the URL to read in the JSON data 
 let link =  'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
-// perform a get request 
+// perform a get request on page load
 d3.json(link).then(function (data){
     console.log(data);
     makeDropdown(data);
     makeMetadata(data, data.names[0]);
     makeBarChart(data, data.names[0]);
+    makeBubbleChart(data, data.names[0]);
+    makeGaugeChart(data, data.names[0]);
 });
 
 function optionChanged(val) {
@@ -14,6 +16,8 @@ function optionChanged(val) {
       console.log(data);
   makeMetadata(data, val);
       makeBarChart(data, val);
+      makeBubbleChart(data, val);
+      makeGaugeChart(data, val);
     });
   }
   function makeDropdown(data) {
@@ -51,7 +55,7 @@ function optionChanged(val) {
       hovertext: otu_labels,
       type: 'bar',
       orientation: "h",
-      marker: {color: 'blue'}
+      marker: {color: '#fcf300'}
     };
   // Data array
     let plotly_data = [trace1];
@@ -64,5 +68,85 @@ function optionChanged(val) {
     Plotly.newPlot("bar", plotly_data, layout);
   }
   
-  
+  function makeBubbleChart(data, val) {
+    let sample = data.samples.filter(x => x.id == val)[0];
 
+  
+    let sample_values = sample.sample_values;
+    let otu_labels = sample.otu_labels;
+    let otu_ids = sample.otu_ids;
+
+
+  // Trace for the bubble chart
+    let trace1 = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        color: otu_ids,
+        size: sample_values,
+        colorscale:'Jet',
+        type:'heatmap'
+      }
+    };
+
+    
+  // Data array
+    let plotly_data = [trace1];
+
+  // Apply a title to the layout
+    let layout = {
+      "title": `Number of Bacteria for ID:${val}`,
+      "xaxis": {'title': "OTU ID"},
+      "yaxis": {'title': "Number of Bacteria Identified"}
+    }
+
+  // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("bubble", plotly_data, layout);
+  }
+// Make the Gauge Chart 
+  function makeGaugeChart(data, val) {
+    let meta = data.metadata.filter(x => x.id == val)[0];
+    let wfreq = meta.wfreq;
+
+    let wfreq_avg = data.metadata.map(x => x.wfreq).reduce((a,b)=> a+b)/data.metadata.length;
+    wfreq_avg = wfreq_avg.toFixed(0);//round
+
+  // Trace for the bubble chart
+    let trace1 = {
+      domain: { x: [0, 1], y: [0, 1] },
+    value: wfreq,
+    title: { text: "" },
+    type: "indicator",
+    mode: "gauge+number+delta",
+    delta: { reference: wfreq_avg},
+    gauge: {
+      axis: { range: [0, 10] },
+      steps: [
+        { range: [0, 5], color: "lightgray" },
+        { range: [5, 7], color: "gray" }
+      ],
+      threshold: {
+        line: { color: "red", width: 4 },
+        thickness: 0.75,
+        value: 9.8
+      }
+    }
+    };
+
+    
+  // Data array
+    let plotly_data = [trace1];
+
+  // Apply a title to the layout
+    let layout = {
+      "title": `Belly Button Washing Frequency
+      <BR>
+      Scrubs per Week`,
+    
+    }
+
+  // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("gauge", plotly_data, layout);
+  }
